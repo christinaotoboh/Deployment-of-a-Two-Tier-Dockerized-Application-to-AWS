9ca99d56c780d372bfaf4763dc2c3c49268e2cac
Christiana Otoboh <otobohsmiles@gmail.com>
2026-03-17 21:06:45 +0100

    Updated provision.sh

--- provision.sh
+++ provision.sh
@@ -1,32 +1,13 @@
 #!/bin/bash
 
-
-# Check if docker is installed on system
-
-systemctl is-active --quiet docker && echo docker is running
-
-# if [systemctl status docker == 1];
-#   echo "docker is installed"
-# fi
-
-
-# STATUS="$(systemctl is-active tomcat.service)"
-# if [ "${STATUS}" = "active" ]; then
-#     echo "Execute your tasks ....."
-# else 
-#     echo " Service not running.... so exiting "  
-#     exit 1  
-# fi
-
-
 sudo apt update
 sudo apt install ca-certificates curl gnupg lsb-release -y
 
 #Add Docker's official GPG key
-
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
 
 # Set up the Docker apt repository
 echo \
@@ -35,38 +16,28 @@ echo \
 
 #Install the Docker Engine packages
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
-sudo apt-get install -y docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
 
 # Add dcoker user to docker group (so no sudo needed)
-usermod -aG docker ubuntu
+sudo usermod -aG docker ubuntu
 
   # Enable and start Docker
-    systemctl enable docker
-    systemctl start docker
+sudo systemctl enable docker
+sudo systemctl start docker
 
-# Mount the EBS Volume on the EC2 instance
+# Format the EBS volume and make it a filesystem 
+sudo mkfs -t ext4 /dev/nvme1n1
 
-# Format the volume with an ext4 filesystem
-sudo mkfs -t ext4 /dev/xvdf
+#Create a mount point where the EBS Volume will appear
+sudo mkdir -p /mnt/ebs
 
-sudo cp -r /mysql-data  /tmp/mysql-data-backup
+#Mount the volume to the mount point (Folder)
+sudo mount /dev/nvme1n1 /mnt/ebs
 
+# Create a folder on the mounted EBS volume for mysql to write to
+sudo mkdir -p /mnt/ebs/mysql-data
 
-mount the mysql-data to the ebs volume
-sudo mount /dev/xvdf /mnt/mysql-data
-
-
-#Set correct ownership/permissions on the mount point so Docker can write to it
-sudo chmod 777 /dev/xvdf
-
-# Install AWS Cli
-
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o"awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-aws configure #Figure out how to automate 
-
-
-
+# mysql runs as a user with UID:999, grant it ownership to the volume directory so that it can write into it
+sudo chown -R 999:999 /mnt/ebs/mysql-data
 
